8a365f2a5594288664540d23e885ea862534c29c
Paris Sfikouris <p.sfikouris@ceranext.com>
2022-08-14 11:15:58 +0300

    retrive tmsi

--- parser.py
+++ parser.py
@@ -4,5 +4,16 @@ import test_module
 cap_gmm = pyshark.FileCapture('gsm_multiple_attach_detach_request.pcapng', display_filter='gsm_a.dtap.msg_gmm_type')
 cap_mm = pyshark.FileCapture('gsm_multiple_attach_detach_request.pcapng', display_filter='gsm_a.dtap.msg_mm_type')
 
+
+# print(cap_mm[0]["gsm_a.dtap"]._all_fields)
+# print(cap_mm[0]['gsm_a.dtap'].msg_mm_type)
+
+# print(dir(cap_mm[0]))
+
+print(dir(cap_mm[0]['gsm_a.dtap']))
+print(cap_mm[0]['gsm_a.dtap'].gsm_a_tmsi)
+
 for packet in cap_mm:
-   test_module.check_gsm_msg_mm_type(packet["gsm_a.dtap"].msg_mm_type)
+   #test_module.handle_gsm_mm_packet(packet["gsm_a.dtap"])
+   #print(packet._all_fields)
+   break
--- test_module.py
+++ test_module.py
@@ -8,21 +8,35 @@ class GSM_MSG_MM_TYPE(enum.Enum):
     Location_Updating_Accept = "0x00000002"
     Authentication_Request = "0x00000012"
     Authentication_Response = "0x00000014"
+    MM_Information = "0x00000032"
 
-def check_gsm_msg_mm_type(gsm_msg):
-    if gsm_msg == GSM_MSG_MM_TYPE.IMSI_Detach_Indication.value:
+class SCORE_BOARD(enum.Enum):
+    Points_IMSI_Detach_Indication_TMSI = 10
+    Points_IMSI_Detach_Indication_IMSI = -10
+    Points_Identify_Request_IMSI = -5 #probably this is invalid. Identify req is always imsi.
+    Points_Identify_Request_TIMSI = 5 #probably this is invalid. Identify req is always imsi.
+    Points_Location_Updating_Request_IMSI = -10
+    Points_Location_Updating_Request_TIMSI = 10 
+    Points_Authentication_Request = 10
+    Points_Authentication_Request_not_asking = -10 #check if it's asking (imsi catcher)
+
+def handle_gsm_mm_packet(packet):
+    if packet.msg_mm_type == GSM_MSG_MM_TYPE.IMSI_Detach_Indication.value:
         print("IMSI Detach Indication received")
-    elif gsm_msg == GSM_MSG_MM_TYPE.Identify_Request.value:
+        #print(packet.gsm_a.tmsi)
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.Identify_Request.value:
         print("Identify Request received")
-    elif gsm_msg == GSM_MSG_MM_TYPE.Identify_Response.value:
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.Identify_Response.value:
         print("Identify Response received")
-    elif gsm_msg == GSM_MSG_MM_TYPE.Location_Updating_Request.value:
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.Location_Updating_Request.value:
         print("Location Updating Request received")
-    elif gsm_msg == GSM_MSG_MM_TYPE.Location_Updating_Accept.value:
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.Location_Updating_Accept.value:
         print("Location Updating Accept received")        
-    elif gsm_msg == GSM_MSG_MM_TYPE.Authentication_Request.value:
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.Authentication_Request.value:
         print("Authentication Request received")    
-    elif gsm_msg == GSM_MSG_MM_TYPE.Authentication_Response.value:
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.Authentication_Response.value:
         print("Authentication Response received")
+    elif packet.msg_mm_type == GSM_MSG_MM_TYPE.MM_Information.value:
+        print("MM_Information received")
     else:
-        print("Unknown mm type ", gsm_msg)    
+        print("Unknown mm type ", packet.msg_mm_type)    
